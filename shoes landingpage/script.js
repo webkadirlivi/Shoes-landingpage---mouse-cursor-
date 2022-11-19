28093239aaf28d5dd5e5cc9280b79e6c40ed55cf
normzl <110020264+normzl@users.noreply.github.com>
2022-11-19 17:59:25 +0300

    landingpage | mouse cursor
    landingpage | mouse cursor

---
+++ shoes landingpage/script.js
@@ -0,0 +1,29 @@
+var logo = document.querySelector('.wrapper-container');
+var menu = document.querySelector('.menu');
+
+logo.addEventListener('click' ,function (){
+    menu.classList.toggle('showmenu')
+});
+
+
+// button hover 
+
+(function() {
+    const buttons = document.querySelectorAll(".btn-posnawr");
+  
+    buttons.forEach(button => {
+      ["mouseenter", "mouseout"].forEach(evt => {
+        button.addEventListener(evt, e => {
+          let parentOffset = button.getBoundingClientRect(),
+              relX = e.pageX - parentOffset.left,
+              relY = e.pageY - parentOffset.top;
+  
+          const span = button.getElementsByTagName("span");
+  
+          span[0].style.top = relY + "px";
+          span[0].style.left = relX + "px";
+        });
+      });
+    });
+  })();
+
